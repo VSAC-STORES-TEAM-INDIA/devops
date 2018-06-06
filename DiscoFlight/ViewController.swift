7ff45da2418c02bb22a9688ef0b49fa2e33ab6ad
MFPDiscoPC <mfpdiscopc@MFPDiscoPCs-MacBook-Pro.local>
2018-06-06 11:48:43 +0530

    initial checkin

---
+++ DiscoFlight/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  DiscoFlight
+//
+//  Created by MFPDiscoPC on 25/05/18.
+//  Copyright © 2018 disco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        //versionLabel
+//        
+//        let versionLabel = UILabel();
+//        versionLabel.center = CGPoint(x:120, y:200);
+//        versionLabel.textAlignment = .center;
+//        versionLabel.text = "some random text";
+//        self.view.addSubview(versionLabel);
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
